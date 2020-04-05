37b94459fd77176d00a7cde99f0cff7c6b03a4e4
Salman Khan <greate43.sk@gmail.com>
2020-04-05 21:15:35 +1000

    setting up project

---
+++ InfosysAssement/app/src/main/java/com/greate43/sk/infosysassement/view/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.greate43.sk.infosysassement.view.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.greate43.sk.infosysassement.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
